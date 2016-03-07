df992cf0323ab0a0101f3524129682da299b3014
karvonz <vincent.gleyzes@gmail.com>
2016-03-07 11:38:22 +0100

    Added files via upload

---
+++ FSM.vhd
@@ -0,0 +1,48 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07.03.2016 11:43:23
+-- Design Name: 
+-- Module Name: FSM - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity FSM is
+    Port ( clock : in STD_LOGIC;
+           reset : in STD_LOGIC;
+           done : in STD_LOGIC;
+           start : out STD_LOGIC;
+           x : out STD_LOGIC_VECTOR (31 downto 0);
+           y : out STD_LOGIC_VECTOR (31 downto 0));
+end FSM;
+
+architecture Behavioral of FSM is
+
+begin
+
+
+end Behavioral;
