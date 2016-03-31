7fd910a21db8e77df97c74cda488612ea02c40bc
Kinxil <anthony_mayorserra@yahoo.fr>
2016-03-31 15:05:49 +0200

    Test

--- Shared.vhd
+++ Shared.vhd
@@ -2,6 +2,8 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
+-- Test
+
 package CONSTANTS is
 	-- Fixed format --
 	constant FIXED : INTEGER := 28; --Number of bits for , part
